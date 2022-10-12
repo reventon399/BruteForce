85af4f6c5ea2d6c4ecfcbeb62f9d6e8200cb1fa2
Alexey <alexeylosev1501@gmail.com>
2022-10-12 22:48:40 +0300

    add setupHierarchy and setupLayout methods

--- BruteForce/Sources/ViewController.swift
+++ BruteForce/Sources/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
     
     var isBlack: Bool = false {
         didSet {
@@ -53,16 +52,7 @@ class ViewController: UIViewController {
         isBlack.toggle()
     }
     
-    //MARK: - Lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        self.bruteForce(passwordToUnlock: "1!gr")
-        
-        // Do any additional setup after loading the view.
-    }
+ //MARK: - BruteForce methods
     
     func bruteForce(passwordToUnlock: String) {
         let ALLOWED_CHARACTERS: [String] = String().printable.map { String($0) }
@@ -79,31 +69,54 @@ class ViewController: UIViewController {
         
         print(password)
     }
-}
-
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index]) : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
     
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
+    func indexOf(character: Character, _ array: [String]) -> Int {
+        return array.firstIndex(of: String(character))!
     }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+    func characterAt(index: Int, _ array: [String]) -> Character {
+        return index < array.count ? Character(array[index]) : Character("")
+    }
+
+    func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
         
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
         }
+        else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+            
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+        
+        return str
+    }
+
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+        self.bruteForce(passwordToUnlock: "1!gr")
     }
     
-    return str
+    //MARK: - Setup
+    
+    private func setupHierarchy() {
+        view.addSubview(changeColorButton)
+        view.addSubview(anotherOneButton)
+        view.addSubview(textField)
+        view.addSubview(label)
+    }
+    
+    private func setupLayout() {
+        
+    }
 }
+
 
